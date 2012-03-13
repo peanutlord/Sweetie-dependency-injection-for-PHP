496cacd5cc5157da3d47de81d59a9c58b98418b4
Christopher Marchfelder <marchfelder@googlemail.com>
2012-03-13 17:27:11 +0100

    Shell script now takes a file/folder as argument

--- tests/run.sh
+++ tests/run.sh
@@ -1,1 +1,13 @@
-phpunit --stop-on-failure --colors --bootstrap "bootstrap.php" --include-path "." src/
+if [ -n $1 ]; then
+	folder="src/"
+else
+	folder=$1
+fi
+
+if [ -d $folder ]; then
+	phpunit --stop-on-failure --bootstrap "bootstrap.php" $folder
+else
+	echo "File or folder $folder does not exist"
+	exit
+fi
+
