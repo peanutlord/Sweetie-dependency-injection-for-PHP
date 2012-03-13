527bb294eeba9de94f8f61ed474f53e062d6e97e
Christopher Marchfelder <marchfelder@googlemail.com>
2012-03-13 17:48:16 +0100

    Shell script should now work as intended

--- tests/run.sh
+++ tests/run.sh
@@ -1,13 +1,13 @@
-if [ -n $1 ]; then
+if [ -z $1 ]; then
 	folder="src/"
 else
 	folder=$1
 fi
 
-if [ -d $folder ]; then
+if [ -f "$PWD/$folder" ] || [ -d "$PWD/$folder" ]; then
 	phpunit --stop-on-failure --bootstrap "bootstrap.php" $folder
 else
-	echo "File or folder $folder does not exist"
+	echo "File or folder $PWD/$folder does not exist"
 	exit
 fi
 
